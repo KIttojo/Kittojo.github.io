be8e3bc9c1bd278175946613c5147f702bc59c35
Kittojo <45813478+KIttojo@users.noreply.github.com>
2020-10-09 20:40:34 +0300

    Posts update
    Now you can see new post you've just made

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 "use strict";
 
+let currentdate = new Date();
+let dateNow = currentdate.getDate() + '.'
+            + (currentdate.getMonth() + 1) + '.'
+            + currentdate.getFullYear();
+
 const DataBase = [];
 
 const addPostBtn = document.querySelector('.add-post-btn'),
@@ -7,11 +12,31 @@ const addPostBtn = document.querySelector('.add-post-btn'),
     navElements = document.querySelectorAll('.nav-element'),
     sideBar = document.querySelector('.side-bar'),
     addPost = document.querySelector('.add-post '),
+    newPostText = document.querySelector('.new-post-text'),
     modalSubmit = document.querySelector('.modal__submit'),
     publishBtn = document.querySelector('.publish');
 
 const navElementsArray = Array.from(navElements);
 const modalSubmitElems = [...modalSubmit.elements].filter((elem) => elem.tagName != 'BUTTON');
+
+const renderPosts = () => {
+    DataBase.forEach(element => {
+        console.log(element);
+        content.insertAdjacentHTML('beforeend', `
+        <div class="post" id="${element.id}" onmouseover="updateViews(id);">
+        <div class="post-line"></div>
+        <div class="post-content">
+            <div class="article-name">${element.PostTitle}</div>
+            <div class="article-content">${element.PostText}</div>
+    
+            <div class="post-info">
+                <span class="article-info">v: 5</span>
+                <span class="article-info">d: ${element.date}</span>
+            </div>
+        </div>
+        `)
+    });
+};
 
 addPostBtn.onclick = function() {
     content.classList.toggle('hide');
@@ -35,6 +60,16 @@ modalSubmit.addEventListener('input', () => {
     validForm ? publishBtn.classList.remove('disabled') : publishBtn.classList.add('disabled');
 });
 
+newPostText.addEventListener('click', () => {
+    if (newPostText.scrollHeight > newPostText.clientHeight) {
+        let height = window.getComputedStyle(newPostText).height;
+        let newHeight = String(Number(height.replace('px', '')) + 25) + 'px';
+        
+        newPostText.style.height = newHeight;
+        console.log(newHeight);
+    }
+}); //нужно написать функцию отдельно и повесить её на элемент + закрытие при отправке формы + написать else при удалении текста
+
 modalSubmit.addEventListener('submit', event => {
     event.preventDefault();
     
@@ -44,10 +79,17 @@ modalSubmit.addEventListener('submit', event => {
         itemObject[elem.name] = elem.value;
     }
     itemObject["id"] = DataBase.length + 100;
+    itemObject["date"] = dateNow;
     DataBase.push(itemObject);
+    // console.log(DataBase);
 
     modalSubmit.reset();
+
+    renderPosts();
 });
 
-document.cookie = "user=John"; // обновляем только куки с именем 'user'
-console.log(document.cookie); // показываем все куки
+// const updateViews = (id) =>{
+//     console.log(id);
+// };
+
+
